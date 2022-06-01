6ec0e4d32f5a8ea3419107c5b2ea3854eebe6d00
wablopilson <wablopilson@gmail.com>
2022-06-01 09:17:04 +0200

    exercise 01 corrected typosB

--- exercise01/exercise1/service/api.service.js
+++ exercise01/exercise1/service/api.service.js
@@ -4,12 +4,11 @@ module.exports = class ApiService {
 
     constructor() {
         this.axiosApp = axios.create({
-            baseURL: 'http://localhost:3000'
+            baseURL: 'http://messageapp:3000'
         })
     }
 
     createMessage(destination, body) {
-        console.log('llego al api handler')
         return this.axiosApp.post('/message', { destination, body })
     }
 
